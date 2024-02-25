6bcf0f11fd04e29c5e2fb257f725e1124446606f
Wren <OtherwiseJunk@gmail.com>
2024-02-25 11:03:37 -0500

    feat: Add processing for rich text features on post text

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { BskyAgent } from "@atproto/api";
+import { BskyAgent, RichText } from "@atproto/api";
 import * as dotenv from "dotenv";
 import { CronJob } from "cron";
 import * as process from "process";
@@ -19,21 +19,39 @@ let lastPostUri: { uri: string; cid: string } | undefined;
 
 async function postPuzzleInAM() {
   lastPostUri = undefined;
-  await agent.login({
+
+  const altText = await getDailyPuzzleAltText();
+  const imageData = await getDailyPuzzleScreenshot();
+  const postText = new RichText({ text: getPostText() });
+  await postText.detectFacets(agent)
+
+  console.log("Posting puzzle...");
+  console.log(`Alt Text: ${altText}`);
+  console.log(`Post Text: ${postText}`);
+
+  const loginResult = await agent.login({
     identifier: process.env.BLUESKY_USERNAME!,
     password: process.env.BLUESKY_PASSWORD!,
   });
-  const { data } = await agent.uploadBlob(await getDailyPuzzleScreenshot(), {
+
+  if(!loginResult.success){
+    console.log("Login failed!");
+    return;
+  }
+
+  const image = await agent.uploadBlob(imageData, {
     encoding: "image/png",
   });
+  
   lastPostUri = await agent.post({
-    text: getPostText(),
+    text: postText.text,
+    facets: postText.facets,
     embed: {
       $type: "app.bsky.embed.images",
       images: [
         {
-          alt: getDailyPuzzleAltText(),
-          image: data.blob,
+          alt: altText,
+          image: image.data.blob,
           aspectRatio: {
             // a hint to clients
             width: 750,
@@ -60,9 +78,14 @@ async function repostPuzzleInEvening() {
 
 const amPostSchedule = "0 8 * * *"; // Run once every day at 8 AM ET
 const pmRepostSchedule = "0 18 * * *"; // Run once every day at 6 PM ET
+//const debugSchedule = "* */1 * * *"; // Runs every minute, for debugging purposes
 
 const postJob = new CronJob(amPostSchedule, postPuzzleInAM);
 const repostJob = new CronJob(pmRepostSchedule, repostPuzzleInEvening);
 
+postPuzzleInAM();
+
 postJob.start();
 repostJob.start();
+
+console.log('Started Magic: The Griddening Puzzle Poster');
