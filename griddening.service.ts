a66186233c6f91a376ab69fc4607c1289bb03d05
Wren <OtherwiseJunk@gmail.com>
2024-02-24 19:35:25 -0500

    feat: add structure for griddening methods

---
+++ griddening.service.ts
@@ -0,0 +1,14 @@
+export async function getDailyPuzzleScreenshot(){
+
+}
+
+export async function getDailyPuzzleAltText(){
+
+}
+
+export function getPostText(){
+    return `https://magicthegridden.ing
+    
+    #MagicTheGathering
+    #MagicTheGriddening`
+}
