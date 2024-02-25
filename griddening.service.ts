6e31065165e2ce592af1a8649af8db56591b8383
Wren <OtherwiseJunk@gmail.com>
2024-02-25 17:11:53 -0500

    feat: inject text color css, make screenshot wider

--- griddening.service.ts
+++ griddening.service.ts
@@ -36,7 +36,8 @@ export async function getDailyPuzzleScreenshot(): Promise<Uint8Array> {
   const browser = await puppeteer.launch(puppeteerOptions);
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle0' });
-  await page.setViewport({ width: 650, height: 650 });
+  await page.setViewport({ width: 800, height: 650 });
+  await page.addStyleTag({content: '.body{color: white}'})
   await page.evaluate(() =>{
     window.scrollTo(0, document.body.scrollHeight);
   })
