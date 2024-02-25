7804ac1b23d5cef1207f161e5a2dbd123d67be44
Wren <OtherwiseJunk@gmail.com>
2024-02-25 01:57:07 -0500

    feat: Add logic for generating screenshot and alt text

--- griddening.service.ts
+++ griddening.service.ts
@@ -1,14 +1,90 @@
-export async function getDailyPuzzleScreenshot(){
 
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+
+const url = "https://magicthegridden.ing";
+const screenshotPath = "./dailyPuzzle.png";
+const gameStateUrl = 'https://magicthegridden.ing/api/gameState/screenshotPoster';
+export enum ConstraintType {
+  Rarity,
+  Type,
+  ManaValue,
+  Color,
+  Set,
+  Power,
+  Toughness,
+  Artist,
+  CreatureRulesText,
+  CreatureRaceTypes,
+  CreatureJobTypes,
+  ArtifactSubtypes,
+  EnchantmentSubtypes,
+  __LENGTH
 }
 
-export async function getDailyPuzzleAltText(){
+export async function getDailyPuzzleScreenshot(): Promise<Uint8Array> {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.setViewport({ width: 750, height: 750 });
+  await setTimeout(async () =>{
+    await page.screenshot({ path: screenshotPath });
+    await browser.close();
 
+  }, 2000);
+
+  return fs.readFileSync(screenshotPath);
 }
 
-export function getPostText(){
-    return `https://magicthegridden.ing
-    
-    #MagicTheGathering
-    #MagicTheGriddening`
+export async function getDailyPuzzleAltText(): Promise<string> {
+  let response = await fetch(gameStateUrl);
+  let data = await response.json();
+  return generateAltTextFromGameState(data);
+}
+
+export function generateAltTextFromGameState(game): string{
+  const topRowDescOne = getDescriptionForConstraint(game.gameConstraints[0]);
+  const topRowDescTwo = getDescriptionForConstraint(game.gameConstraints[1]);
+  const topRowDescThree = getDescriptionForConstraint(game.gameConstraints[2]);
+  const sideRowDescOne = getDescriptionForConstraint(game.gameConstraints[3]);
+  const sideRowDescTwo = getDescriptionForConstraint(game.gameConstraints[4]);
+  const sideRowDescThree = getDescriptionForConstraint(game.gameConstraints[5]);
+
+  return `A Magic The Griddening Puzzle.
+The puzzle is a 3x3 grid of inputs, with the following constraints:
+Top row: ${topRowDescOne}, ${topRowDescTwo}, ${topRowDescThree}
+Side row: ${sideRowDescOne}, ${sideRowDescTwo}, ${sideRowDescThree}
+The board is blank.`
+}
+
+export function getDescriptionForConstraint(constraint){
+  switch(constraint.constraintType){
+    case ConstraintType.ManaValue:
+      return `${constraint.displayName}`;
+    case ConstraintType.Artist:
+      return `${constraint.displayName}`;
+    case ConstraintType.Power:
+      return `${constraint.displayName}`;
+    case ConstraintType.Toughness:
+      return `${constraint.displayName}`;
+    case ConstraintType.CreatureRulesText:
+      return `${constraint.displayName} (Rules Text)`;
+    case ConstraintType.CreatureRaceTypes:
+      return `${constraint.displayName} (Creature Type)`;
+    case ConstraintType.CreatureJobTypes:
+      return `${constraint.displayName} (Creature Type)`;
+    case ConstraintType.ArtifactSubtypes:
+      return `${constraint.displayName} (Artifact Type)`;
+    case ConstraintType.EnchantmentSubtypes:
+      return `${constraint.displayName} (Enchantment Type)`;
+    default:
+      return `${constraint.displayName} (${ConstraintType[constraint.constraintType]})`;
+  }
+}
+
+export function getPostText() {
+  return `https://magicthegridden.ing
+
+#MagicTheGathering
+#MagicTheGriddening`;
 }
--- index.ts
+++ index.ts
@@ -1,36 +1,65 @@
-import { BskyAgent } from '@atproto/api';
-import * as dotenv from 'dotenv';
-import { CronJob } from 'cron';
-import * as process from 'process';
+import { BskyAgent } from "@atproto/api";
+import * as dotenv from "dotenv";
+import { CronJob } from "cron";
+import * as process from "process";
+import {
+  getDailyPuzzleAltText,
+  getDailyPuzzleScreenshot,
+  getPostText,
+} from "./griddening.service";
 
 dotenv.config();
 
-// Create a Bluesky Agent 
+// Create a Bluesky Agent
 const agent = new BskyAgent({
-    service: 'https://bsky.social',
-  })
+  service: "https://bsky.social",
+});
 
-let lastPostUri: { uri: string, cid: string} | undefined;
-
+let lastPostUri: { uri: string; cid: string } | undefined;
 
 async function postPuzzleInAM() {
-    await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
-    lastPostUri = await agent.post({
-        text: "🙂"
-    });
-    console.log("Just posted!")
+  lastPostUri = undefined;
+  await agent.login({
+    identifier: process.env.BLUESKY_USERNAME!,
+    password: process.env.BLUESKY_PASSWORD!,
+  });
+  const { data } = await agent.uploadBlob(await getDailyPuzzleScreenshot(), {
+    encoding: "image/png",
+  });
+  lastPostUri = await agent.post({
+    text: getPostText(),
+    embed: {
+      $type: "app.bsky.embed.images",
+      images: [
+        {
+          alt: getDailyPuzzleAltText(),
+          image: data.blob,
+          aspectRatio: {
+            // a hint to clients
+            width: 750,
+            height: 750,
+          },
+        },
+      ],
+    },
+    createdAt: new Date().toISOString(),
+  });
+  console.log("Just posted!");
 }
 
 async function repostPuzzleInEvening() {
-    await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
-    if(lastPostUri){
-        await agent.repost(lastPostUri.uri, lastPostUri.cid);
-        console.log("Just reposted!");
-    }
+  await agent.login({
+    identifier: process.env.BLUESKY_USERNAME!,
+    password: process.env.BLUESKY_PASSWORD!,
+  });
+  if (lastPostUri) {
+    await agent.repost(lastPostUri.uri, lastPostUri.cid);
+    console.log("Just reposted!");
+  }
 }
 
-const amPostSchedule = '0 8 * * *'; // Run once every day at 8 AM ET
-const pmRepostSchedule = '0 18 * * *'; // Run once every day at 6 PM ET
+const amPostSchedule = "0 8 * * *"; // Run once every day at 8 AM ET
+const pmRepostSchedule = "0 18 * * *"; // Run once every day at 6 PM ET
 
 const postJob = new CronJob(amPostSchedule, postPuzzleInAM);
 const repostJob = new CronJob(pmRepostSchedule, repostPuzzleInEvening);
